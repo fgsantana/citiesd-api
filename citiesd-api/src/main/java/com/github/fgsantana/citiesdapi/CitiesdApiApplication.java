b77fa31048d4e8644bbc6a7bbc1d99bccd316396
felipe gomes <felipinhogoma@gmail.com>
2020-09-05 04:46:59 -0300

    Start message

--- citiesd-api/src/main/java/com/github/fgsantana/citiesdapi/CitiesdApiApplication.java
+++ citiesd-api/src/main/java/com/github/fgsantana/citiesdapi/CitiesdApiApplication.java
@@ -1,13 +1,25 @@
 package com.github.fgsantana.citiesdapi;
 
+import com.github.fgsantana.citiesdapi.message.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api/v1")
 public class CitiesdApiApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitiesdApiApplication.class, args);
+	}
+
+	@GetMapping
+	public Message start(){
+		return new Message();
 	}
 
 }
